66707e88eb73b8ffa42cd00019661d4346a99b2b
Edison Costa <edisoncosta@lojaplus.com.br>
2015-11-25 16:29:33 -0200

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -14,6 +14,13 @@ wget -qO ee rt.cx/ee && sudo bash ee
 # Instalacao de Servidores
 ee stack install --nginx --php --mysql --phpmyadmin --utils --postfix --wpcli --hhvm --redis
 
+# Instalacao do Composer
+cd /usr/src
+curl -sS https://getcomposer.org/installer | sudo php
+sudo mv composer.phar /usr/bin/composer
+
+# Restart servicos
+ee stack restart
 
 
 
